5d498a0b0d8c33d178107e3953c0d1487150ec5e
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 13:15:40 +0500

    feat: add list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import {join} from 'path'
+import {readdir} from 'fs/promises'
+
 const list = async () => {
     // Write your code here 
+    const folderPath = join('src', 'fs', 'files');
+    try {
+        const files = await readdir(folderPath);
+        console.log(files);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
