367b9f75141ebee44428c53e884e543d82a21fe7
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 11:27:56 +0500

    feat: add copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,16 @@
+import { join } from "path";
+import { cp } from "fs/promises";
+
 const copy = async () => {
     // Write your code here 
+    const sourcePath = join('src','fs', 'files');
+    const destinationPath = join('src', 'fs', 'files_copy');
+    console.log({sourcePath, destinationPath})
+    try {
+        await cp(sourcePath, destinationPath, { recursive: true, errorOnExist: true, force: false });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
