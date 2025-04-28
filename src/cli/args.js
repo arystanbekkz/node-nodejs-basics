4303a6805b6f0bfb791f7d0150f4bd06e7875d8c
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 15:54:56 +0500

    refactor: args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,8 +3,9 @@ const parseArgs = () => {
     const allArgs = process.argv
     const args = allArgs.slice(2)
     const result = [];
-    for (const argument of args) {
-        const [prop, value] = argument.replace('--', '').split('=')
+    for (let i = 0; i < args.length; i += 2) {
+        const prop = args[i].replace(/^--/, '')
+        const value = args[i+1]
         result.push(`${prop} is ${value}`)
     }
     console.log(result.join(', '))
