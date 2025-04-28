4f78a0235a6a3aef20f034cb14a7b70ced716f0c
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 18:30:19 +0500

    feat: add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = join('src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+    console.log({filePath})
+    try {
+        const readStream = createReadStream(filePath);
+        const hash = createHash('sha256');
+        await pipeline(readStream, hash);
+        const hashResult = hash.digest('hex');
+        console.log(hashResult)
+    } catch (error) {
+        throw new Error("Error while creating hash: ", error.message)
+    }
 };
 
 await calculateHash();
