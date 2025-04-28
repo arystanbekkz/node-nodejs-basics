dfd982c3133b92c35fe068650fd9b1d7113d8feb
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 12:07:10 +0500

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { join } from "path";
+import { unlink } from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+    // Write your code here
+    const filePath = join('src','fs', 'files','fileToRemove.txt');
+    try {
+        await unlink(filePath)
+    } catch(error) {
+        throw new Error("FS operation failed")
+    }
 };
 
 await remove();
