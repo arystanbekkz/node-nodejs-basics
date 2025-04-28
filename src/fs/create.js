dac914bea67380fa79fca09b85c0da633d8c872e
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-04-28 11:23:01 +0500

    feat: add function create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+
 const create = async () => {
     // Write your code here 
+    const filePath = join('src', 'fs', 'files', 'fresh.txt');
+    console.log({filePath})
+    try {
+        await writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
